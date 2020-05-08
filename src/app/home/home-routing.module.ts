46aeaaf83a893d05c967659eb5c9c1a4c4d06dfa
Adrian Ruiz <flamekasai@gmail.com>
2020-05-09 01:30:04 +0200

    Added basic routing structure

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,8 +4,35 @@ import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     component: HomePage,
+    children: [
+      {
+        path: 'campaings',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../campaings/campaings.module').then( m => m.CampaingsPageModule)
+          },
+          {
+            path: 'new-campaing',
+            loadChildren: () => import('../campaings/new-campaing/new-campaing.module').then(m => m.NewCampaingPageModule)
+          },
+          {
+            path: 'edit-campaing/:campaingId',
+            loadChildren: () => import('../campaings/edit-campaing/edit-campaing.module').then(m => m.EditCampaingPageModule)
+          },
+          {
+            path: ':campaingId',
+            loadChildren: () => import('../campaings/campaing-details/campaing-details.module').then(m => m.CampaingDetailsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'account',
+        loadChildren: () => import('../account/account.module').then(m => m.AccountPageModule)
+      }
+    ]
   }
 ];
 
