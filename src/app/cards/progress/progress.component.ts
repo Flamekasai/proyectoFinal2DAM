dfa1b68f8478bb5ad758180b916c495ce2c6697a
Adrian Ruiz <flamekasai@gmail.com>
2020-06-18 22:57:21 +0200

    Added progress component

--- src/app/cards/progress/progress.component.ts
+++ src/app/cards/progress/progress.component.ts
@@ -9,9 +9,30 @@ import { Card } from '../card-item';
   styleUrls: ['./progress.component.scss'],
 })
 export class ProgressComponent extends CardImplementation implements OnInit {
+  private step: number = 0.1;
 
   constructor() { super(); }
 
   ngOnInit() {}
 
+  initDefaults(type: string) {
+    super.initDefaults(type);
+    this.data.value = 0.5;
+  }
+
+  incrementValue() {
+    let result = Number.parseFloat((this.data.value + this.step).toFixed(2));
+    if (result <= 1) {
+      this.data.value = result;
+      console.log(result);
+    }
+  }
+
+  decrementValue() {
+    let result = Number.parseFloat((this.data.value - this.step).toFixed(2));
+    if (result >= 0) {
+      this.data.value = result;
+      console.log(result);
+    }
+  }
 }
